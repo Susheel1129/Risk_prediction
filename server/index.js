d500878d13176be18ef66a3f508a3927abc14a7d
Susheel1129 <22cs01058@iitbbs.ac.in.>
2025-08-06 20:00:06 +0530

    cors

--- server/index.js
+++ server/index.js
@@ -5,10 +5,35 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+
+const allowedOrigins = [ 
+    'https://www.airfetch.online',
+    'https://airfetch.online',  // Production frontend
+    'https://api.airfetch.online', // Production API
+    'http://localhost:5173', // Development frontend
+  ];
+  console.log(allowedOrigins);
+  app.use(cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like curl or Postman)
+      if (!origin) return callback(null, true);
+  
+      if (allowedOrigins.indexOf(origin) !== -1) {
+        // Origin is allowed
+        callback(null, true);
+      } else {
+        // Origin is not allowed
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }));
+
+app.use(cookieParser());
 app.use(express.json());
-
-
+app.use(express.urlencoded({ extended: true }));
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
